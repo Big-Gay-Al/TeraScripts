dadf8a3695a66a0eafdf7e9690fcd09d0e8e163c
Kiive <ferrari.marco.a@gmail.com>
2017-03-01 23:51:52 -0300

    Update skills_sequences.js

--- skills_sequences.js
+++ skills_sequences.js
@@ -23,9 +23,9 @@
     
     
  //RA sequence
-    //c_press_skill
-    //s_abnormality_begin
-    //s_player_stat_update
+    //c_press_skill             <<start casting ra
+    //s_abnormality_begin       <<sequential fire avaible abnormal
+    //s_player_stat_update     
     //s_player_change_all_prof
     //S_F2P_PremiumUser_Permission
     //s_action_stage
@@ -36,17 +36,17 @@
     //s_action_stage
     //s_action_stage
     //s_action_change_hp <<<< start overcharge
-    //s_action_change_hp
-    //s_action_change_hp
-    //c_press_skill
-    //s_create_change_hp
-    //s_start_cooltime_skill
-    //s_grant_skill
-    //s_abnormality_end
+    //s_action_change_hp  <<<2nd hp tick
+    //s_action_change_hp   <<<3rd hp tick
+    //c_press_skill         <<release ra
+    //s_create_change_hp    <<normal hp regen
+    //s_start_cooltime_skill    <<start ra cd
+    //s_grant_skill             <<<??? maybe casting sq
+    //s_abnormality_end         ´<<finish sq buff    
     //s_player_stat_update
     //s_player_change_all_prof
     //S_F2P_PremiumUser_Permission
-    //c_start_instance_skill
+    //c_start_instance_skillww
     //s_action_stage
     //s_instance_arrow
     //c_start_instance_skill
@@ -59,7 +59,34 @@
     //s_start_cooltime_skill
     //s_action_end
     //s_creature_change_hp
+
+
+//if ra canceled mid charge, after s_creatira_change_hp
+    //c_cancel_skill
+    //c_start_instance_skill
+    //c_press_skill
+    //s_creatura_change_hp
+    //s_action_end
+    //s_abnormality_end
+    //s_player_stat_update
+    //s_player_change_all_prof
+    //S_F2P_PremiumUser_Permission
+    //s_skill_category
+    //s_abnormality_end
+    //s_action_stage
+    //s_instance_arrow
+    //s_start_cooltime_skill
+    //s_action_end
+    //s_creature_change_hp
+    //s_creature_change_hp
+//only sequential fire
+    //s_skill_category
+    //s_abnormality_end
+    //S_action_stage
+    //s_instance_arrow
+    //s_start_cooltime_skill
+    //s_action_end
     
-    
+
     
     
