3ba58fb4a88f9f4311407ca95d5cea7c22f10176
Kiive <ferrari.marco.a@gmail.com>
2017-03-02 13:50:27 -0300

    Update AutoSq.js

--- AutoSq.js
+++ AutoSq.js
@@ -3,92 +3,95 @@ const BUFF_FOCUS = 601450;
 const JOB_ARCHER = 5;
 
 
+
 const SKILL_RADIANT_ARROW = 3;
 const SKILL_SEQUENTIAL_FIRE = 22;
 const SKILL_THUNDERBOLT = 29;
 
-
 module.exports = function AutoSq(dispatch) {
-  let cid;
-  let job;
-  let model;
-  let enabled = false;
+    let cid;
+    let job;
+    let model;
+    let enabled = false;
 
-  let aspd = 1;
-  let atkid = 0x7F000000;
-  let sent = {};
-  let attacks = {};
+    let aspd = 1;
+    let atkid = 0x7F000000;
+    let sent = {};
+    let attacks = {};
 
-  let buff = 0;
+    let buff = 0;
 
-  dispatch.hook('sLogin', (event) => {
-    ({cid, model} = event);
+    dispatch.hook('sLogin', (event) => {
+        ({
+            cid,
+            model
+        } = event);
 
-    job = (model - 10101) % 100;
-    enabled = [JOB_ARCHER].includes(job);
-  });
+        job = (model - 10101) % 100;
+        enabled = [JOB_ARCHER].includes(job);
+    });
 
-  dispatch.hook('sPlayerStatUpdate', (event) => {
-    aspd = 1 + (event.bonusAttackSpeed / event.baseAttackSpeed);
-  });
+    dispatch.hook('sPlayerStatUpdate', (event) => {
+        aspd = 1 + (event.bonusAttackSpeed / event.baseAttackSpeed);
+    });
 
-  dispatch.hook('sAbnormalityBegin', (event) => {
-    if (!enabled || !event.target.equals(cid)) return;
+    dispatch.hook('sAbnormalityBegin', (event) => {
+        if (!enabled || !event.target.equals(cid)) return;
 
-    if (job === JOB_ARCHER && event.id === BUFF_FOCUS) {
-      buff = Date.now() + event.duration;
-    }
-  });
+        if (job === JOB_ARCHER && event.id === BUFF_FOCUS) {
+            buff = Date.now() + event.duration;
+        }
+    });
 
-  dispatch.hook('sAbnormalityRefresh', (event) => {
-    if (!enabled || !event.target.equals(cid)) return;
+    dispatch.hook('sAbnormalityRefresh', (event) => {
+        if (!enabled || !event.target.equals(cid)) return;
 
-    if (job === JOB_ARCHER && event.id === BUFF_FOCUS) {
-      buff = Date.now() + event.duration;
-    }
-  });
+        if (job === JOB_ARCHER && event.id === BUFF_FOCUS) {
+            buff = Date.now() + event.duration;
+        }
+    });
 
-  dispatch.hook('sAbnormalityEnd', (event) => {
-    if (!enabled || !event.target.equals(cid)) return;
-  }
-});
-  
-//FOR THUNDERBOLT  
-dispatch.hook('cStartSkill', (event) => {
-    if (!enabled || job !== JOB_ARCHER) return;
-
-    const skill = event.skill - 0x4000000;
-    const group = Math.floor(skill / 10000);
-    const hit = skill % 100;
-
-    if (group !== SKILL_THUNDERBOLT) return;
-
-    const speed = aspd;
-    const tbduration=3000 //duration of thunderbolt animation
-    const ping = 180
-  
-
-    setTimeout(tbsq, (tbduration / speed)-ping,event);
+    dispatch.hook('sAbnormalityEnd', (event) => {
+        if (!enabled || !event.target.equals(cid)) return;
 
 
-  });
-  
-        function tbsq(event) {
-          
-          dispatch.toServer('cStartInstanceSkill', {
-          skill: 67329664
-          x: event.x || event.x1,
-          y: event.y || event.y1,
-          z: event.z || event.z1,
-          w: event.w,
-          unk: 0,
-          targets:[],  
+
 
     });
 
+    dispatch.hook('sActionStage', (event) => {
+        if (!enabled || job !== JOB_ARCHER) return;
+
+        const skill = event.skill - 0x4000000;
+        const group = Math.floor(skill / 10000);
+        const hit = skill % 100;
+
+        if (group !== SKILL_THUNDERBOLT) return;
+
+        const speed = aspd;
+        const tbduration = 3000 //duration of thunderbolt animation
+        const ping = 180
+	 const delay= tbduration / speed
+
+        setTimeout(function(event){tbsq(event);},delay, event);
 
 
-  }
+    });
+
+    function tbsq(event) {
+
+        dispatch.toServer('cStartInstanceSkill', {
+            skill: 67329664,
+            x: event.x1,
+            y: event.y1,
+            z: event.z1,
+            w: event.w,
+            unk: 0,
+            targets: [],
+	    locs:[{x:0, y:0, z:0}],
+        });
+
+
+
+    }
 };
-    
-    
